a2c01c2472d03de80e75b4613d64454f808e6a30
joshyt100 <joshyt100@gmail.com>
2024-12-21 14:31:27 -0500

    debugging

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const navigate = useNavigate();
   console.log("env-service-id", import.meta.env.VITE_SERVICE_ID);
-  console.log("env-template-id", import.meta.env.VITE_SERVICE_ID);
+  console.log("env-template-id", import.meta.env.VITE_TEMPLATE_ID);
   console.log("env-public-key", import.meta.env.VITE_PUBLIC_KEY);
 
   const sendEmail = (e) => {
